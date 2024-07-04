d0bc3b8d5aa286389b9a82f2d43fa6558fd5abf6
MdShuvo18 <mdks33881@gmail.com>
2024-07-04 14:55:19 +0600

    some edited

--- src/Component/Details/AllProjects.jsx
+++ src/Component/Details/AllProjects.jsx
@@ -37,7 +37,7 @@ const AllProjects = () => {
 
                                 </Link>
                                 <Link target="_blank" to='https://github.com/MdShuvo18/LearnBridge'>
-                                    <div className='ml-12 w-10 h-10'>
+                                    <div className='ml-8 w-10 h-10'>
                                         <img src="/github-icon-2.svg" alt="" />
                                     </div>
                                 </Link>
@@ -76,7 +76,7 @@ const AllProjects = () => {
                                     </Link>
                                 </div>
                                 <Link target="_blank" to='https://github.com/MdShuvo18/PulsePost'>
-                                    <div className='ml-12 w-10 h-10'>
+                                    <div className='ml-8 w-10 h-10'>
                                         <img src="/github-icon-2.svg" alt="" />
                                     </div>
                                 </Link>
@@ -113,7 +113,7 @@ const AllProjects = () => {
 
                                 </Link>
                                 <Link target="_blank" to='https://github.com/MdShuvo18/ArtyPot'>
-                                    <div className='ml-12 w-10 h-10'>
+                                    <div className='ml-8 w-10 h-10'>
                                         <img src="/github-icon-2.svg" alt="" />
                                     </div>
                                 </Link>
